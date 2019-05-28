1e2a083fe06ecc82587edac89197ee93a8b41d99
anamika8 <anamikaeight@gmail.com>
2019-05-27 21:39:53 -0700

    created config file

---
+++ config.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.DATABASE_URL =
+    process.env.DATABASE_URL || "mongodb://localhost/mommy-talks-app";
+exports.TEST_DATABASE_URL =
+    process.env.TEST_DATABASE_URL || "mongodb://localhost/test-mommy-talks-app";
+exports.PORT = process.env.PORT || 8088;
