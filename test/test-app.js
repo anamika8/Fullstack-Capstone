bbf50fe17bac33c4d55c8bd3d71e43deb8db993a
anamika8 <anamikaeight@gmail.com>
2019-05-27 23:29:32 -0700

    updated files to test

--- test/test-app.js
+++ test/test-app.js
@@ -2,16 +2,14 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const faker = require('faker');
-const mongoose = require('mongoose');
+
 
 // this makes the expect syntax available throughout
 // this module
 const expect = chai.expect;
 
-const { Post } = require('../models');
 const { app, runServer, closeServer } = require('../server');
-const { TEST_DATABASE_URL } = require('../config');
+
 
 chai.use(chaiHttp);
 
