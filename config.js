9d97d9042bac5409b9b2e48a04fba02aaa2372de
anamika8 <anamikaeight@gmail.com>
2019-05-27 23:12:01 -0700

    setup database for start

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL =
     process.env.TEST_DATABASE_URL || "mongodb://localhost/test-mommy-talks-app";
 exports.PORT = process.env.PORT || 8088;
+exports.JWT_SECRET = process.env.JWT_SECRET;
